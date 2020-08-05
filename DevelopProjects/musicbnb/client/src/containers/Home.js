f6f9a73adbeb179bde6f157ef273242a4fb81da0
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2020-08-05 12:48:33 +0200

    many new changes

---
+++ DevelopProjects/musicbnb/client/src/containers/Home.js
@@ -0,0 +1,45 @@
+import React, { Component, useState, Link} from 'react';
+import Display from '../components/Display';
+import Header from '../components/Header';
+import styled from 'styled-components';
+
+const Button = styled.a`
+  /* display: inline-block; */
+  border-radius: 3px;
+  padding: 0.5rem 5px;
+  margin: 0.5rem 1rem;
+  width: fit-content;
+  background: pink;
+  color: black;
+  border: 1px solid black;
+  position: relative;
+  /* right: -250px; */
+  float: right;
+`
+
+class Home extends Component  {
+
+    render(){
+
+
+        return(
+            
+            <div className="home">
+                
+                <div className="header">
+                
+                    <Header />
+                    <Button as={Link} href='/add'> Add new </Button>
+
+                </div>
+                
+                <div className="body">
+                    <Display />
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+export default Home
---
+++ DevelopProjects/musicbnb/routes/api.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const Instrument = require('../models/Instrument');
+var moment = require ('moment')
+
+//Get all instruments
+router.get('/', (req, res) => {
+
+
+
+    Instrument.find({}).sort( { date: -1 })
+        .then((data) => {
+            console.log('Data: ', data)
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log('error: ', err)
+        })
+});
+
+//Get specific instrument
+router.get('/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    Instrument.find({ _id: id })
+        .then((data) => {
+            console.log('Data: ', data)
+            res.json(data);
+        })
+        .catch((err) => {
+            console.log('error: ', err)
+        })
+})
+
+router.post('/add', (req, res) => {
+    console.log('Body: ', req.body)
+    const data = req.body;
+
+    const newInstrument = new Instrument(data);
+    newInstrument.save();
+    res.json({
+        msg: "we received your data"
+    })
+});
+
+module.exports = router;
