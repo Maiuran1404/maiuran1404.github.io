47692af6507916b2bc7eead6b954016b235310c8
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2020-08-14 15:44:34 +0200

    first commit

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,23 +10,34 @@ import './Home.scss';
 
 function Home() {
   
-  let app = useRef(null);
-  let images = useRef(null);
-  let content = useRef(null);
-  let tl = new TimelineLite({delay: .3})
+    let app = useRef(null);
+    let images = useRef(null);
+    let content = useRef(null);
+    let tl = new TimelineLite({delay: .3})
+    let buttons = useRef(null);
 
     useEffect(() => {
 
-      //IMAGES VARS
-      const girlImage = images.firstElementChild;
-      const boyImage = images.lastElementChild;
+        //IMAGES VARS
+        const girlImage = images.firstElementChild;
+        const boyImage = images.lastElementChild;
 
-      // Content VARS
-      const headlineFirst = content.children[0].children[0];
-      const headlineSecond = headlineFirst.nextSibling;
-      const headlineThird = headlineSecond.nextSibling;
-      const contentP = content.children[1];
-      const contentButton = content.children[2];
+        // Content VARS
+        const headlineFirst = content.children[0].children[0];
+        const headlineSecond = headlineFirst.nextSibling;
+        const headlineThird = headlineSecond.nextSibling;
+        const contentone = content.children[1];
+        const contenttwo = content.children[2];
+        const contentthree = content.children[3];
+        const contentfour = content.children[4];
+        const contentfive = content.children[5];
+        const contentsix = content.children[6];
+        const conotentseven = content.children[7];
+
+        const buttonsone  = buttons.children[0];
+        const buttonstwo  = buttons.children[1];
+        const buttonsthree  = buttons.children[2];
+        
 
       //Removing flash
       TweenMax.to(app, 0, {css: {visibility: 'visibile'}})
@@ -41,10 +52,18 @@ function Home() {
       //Content Animation
       tl.staggerFrom([headlineFirst.children, headlineSecond.children, headlineThird.children], 1, {
         y: 44,
-        ease: Power3.easeOut, delay: .6
-      }, .15, 'Start')
-      .from(contentP, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.4)
-      .from(contentButton, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.6);
+        ease: Power3.easeOut, delay: .2
+      }, .10, 'Start')
+      .from(contentone, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.4)
+      .from(contenttwo, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.5)
+      .from(contentthree, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.6)
+      .from(contentfour, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.7)
+      .from(contentfive, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.8)
+      .from(contentsix, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.9)
+      .from(conotentseven, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 2.0)
+      .from(buttonsone, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 0.6)
+      .from(buttonstwo, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.0)
+      .from(buttonsthree, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.4);
     
     }, [tl])
 
@@ -54,75 +73,52 @@ function Home() {
         <div className="hero-inner">
           <div className="hero-content">
             <div className="hero-content-inner" ref={el => content = el}>
-              <h1>
+                <h1>
                 <div className="hero-content-line">
-                  <div className="hero-content-line-inner"> Maiuran Loganathan: </div>
+                    <div className="hero-content-line-inner"> Maiuran Loganathan </div>
                 </div>
                 <div className="hero-content-line">
-                  <div className="hero-content-line-inner"> Effectively building solutions to </div>  
+                    <div className="hero-content-line-inner-second"> Effectively building solutions to </div>  
                 </div>
                 <div className="hero-content-line">
-                  <div className="hero-content-line-inner"> meaningful challenges </div>
+                    <div className="hero-content-line-inner-second"> meaningful challenges </div>
                 </div>
-              </h1>
-              <p>
-                Competencies:
-              </p>
-              <p> Full stack: MERN stack, Firebase + MongoDb, React, Gsap </p>
-              {/* <p> Mobile: React Native + Swift </p>
-              <p> UI/UX: Figma, Sketch </p> */}
-              <div >
+                </h1>
+                <p> Full stack: MERN stack, Firebase + MongoDb, React, Gsap </p>
+                <p> Mobile: React Native + Swift </p>
+                <p> UI/UX: Figma, Sketch </p>
+                <br/>
+                
+                </div>
+            </div>
+        <div className="hero-content"  >
+        <div className="hero-content-inner" ref={el => buttons = el}>
+            <div className="btn-row">
                 <button className="explore-button" type="button"
                         onClick={(e) => {
                           e.preventDefault();
                           window.location.href='/coding';
-                          }}> Podcast: 
-                  <div className="arrow-icon">
-                    
-                    {/* <img src={arrow} alt="row"/> */}
-                  </div>
+                          }}> Coding
                 </button>
               </div>
-              <div >
+              <div className="btn-row">
                 <button className="explore-button" type="button"
                         onClick={(e) => {
                           e.preventDefault();
                           window.location.href='/startups';
-                          }}> Podcast: 
-                  <div className="arrow-icon">
-                    
-                    {/* <img src={arrow} alt="row"/> */}
-                  </div>
+                          }}> CV stuff
                 </button>
               </div>
-              <div >
+              <div className="btn-row">
                 <button className="explore-button" type="button"
                         onClick={(e) => {
                           e.preventDefault();
                           window.location.href='/coding';
-                          }}> Podcast: 
-                  <div className="arrow-icon">
-                    
-                    {/* <img src={arrow} alt="row"/> */}
-                  </div>
+                          }}> My story
                 </button>
               </div>
-            </div>
-            
-          </div>
-          {/* <div className="hero-images">
-            <div className="hero-images-inner" ref={el => images = el}>
-              <div className="hero-image girl">
-                <img src={imgGirl} alt="girl" />
-              </div>
-              <div className="hero-image boy">
-                <img src={imgBoy} alt="boy" />
-              </div>
-            </div>
-          </div> */}
         </div>
-        <div className="hero-content-inner">
-            <h1>Second para</h1>
+        </div>
         </div>
       </div>
     </div>
