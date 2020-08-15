041ab672c52884a36892e33c67995df63a5fd69e
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2020-08-15 12:37:11 +0200

    Updates

---
+++ precache-manifest.36b8a57a9fb067b7b1e72329f60367aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934d3c3639e6d0af69d9fbfeec043bc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d428ea81f3900d93f926",
+    "url": "/static/css/main.252b28b2.chunk.css"
+  },
+  {
+    "revision": "5001406b238a8c36e514",
+    "url": "/static/js/2.47cba0a3.chunk.js"
+  },
+  {
+    "revision": "0fb1ab9166e4090d40b886200af7baec",
+    "url": "/static/js/2.47cba0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d428ea81f3900d93f926",
+    "url": "/static/js/main.64cd6076.chunk.js"
+  },
+  {
+    "revision": "32b22702bece1725e19d",
+    "url": "/static/js/runtime-main.3e403037.js"
+  }
+]);
