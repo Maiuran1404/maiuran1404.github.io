fca349595a3ec1c7552bc43083cc6784a6b0675d
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2020-08-15 15:55:01 +0200

    mayoo saturday commmit

--- src/containers/Programming.js
+++ src/containers/Programming.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+    gap: 30px 30px;
+    grid-template-areas: "Title Title" ". .";
+`
+
+const GreyBox = styled.div`
+    background: grey;
+`
+
+const Title = styled.div`
+    grid-area: Title;
+`
 
 class Programming extends Component {
     render() {
         return (
-            <div>
-                <h1> Coding </h1>
-            </div>
+            <Container>
+                <Title> Coding </Title>
+                <GreyBox>
+                    <h2>Delta</h2>
+                </GreyBox>
+                <GreyBox>
+                    <h2>MusicBnB</h2>
+                </GreyBox>
+            </Container>
         )
     }
 }
